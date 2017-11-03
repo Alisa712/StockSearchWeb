b56269701a1e3c472c93042757662393dcfa0a33
ShuhuiHe <hsh0712@hotmail.com>
2017-11-03 16:24:37 -0700

    left table

--- js/stock.js
+++ js/stock.js
@@ -10,11 +10,16 @@ function mainControl($scope, $http, $interval, $window){
 		$scope.quoted = false;
 		$scope.records = ["Symbol", "Stock Price", "Change (Change Percent)", "Volume"];
 		$scope.sortChoices = ["Default", "Symbol", "Stock Price", "Change", "Change Percent", "Volume"];
+		//$scope.leftTable = ["Stock Ticker Symbol", "Last Price", "Change (Change Percent)", ]
 		$scope.selectSortBy = $scope.sortChoices[0];
 		$scope.orderChoices = ["Ascending", "Descending"];
 		$scope.selectOrderBy = $scope.orderChoices[0];
 		$scope.expression = "cs"; //current stock
 		$scope.stockDetails = {};
+		$scope.downColor = "red";
+		//$scope.downImg = "./img/Up.png";
+		$scope.upColor = "green";
+
 		// $window.localStorage.removeItem("favorite");
 		if ($window.localStorage.getItem("favorite") === null){
 			$window.localStorage.setItem('favorite', JSON.stringify($scope.stockDetails));
@@ -37,32 +42,40 @@ function mainControl($scope, $http, $interval, $window){
 		var params = {symbol: $scope.quoteStockName, function: "TIME_SERIES_DAILY"};
 		$http.get(URL, {params: params}).then(function(res) {
 			console.log(res);
+			var timeSeriesDaily = [];
+			var timeSet = res.data["Time Series (Daily)"];
+
+			angular.forEach(timeSet, function(value, key) {
+			  	this.push(value);
+			}, timeSeriesDaily);
+
+			var previousDate = timeSeriesDaily[1];
 			var currentDateTemp = res.data["Meta Data"]["3. Last Refreshed"];
+			var closedTime = "16:00:00"; 
 			var currentDate = currentDateTemp.substring(0, 10); //yyyy-mm-dd
-			var temp = res.data["Time Series (Daily)"][currentDate];
+			var temp = timeSet[currentDate];
 			var submit = {};
 			submit['symbol'] = $scope.quoteStockName;
-			submit['close'] = temp["4. close"];
 			submit['volume'] = temp["5. volume"];
 			submit['change'] = (temp["4. close"] - temp["1. open"]).toFixed(2);
-			submit['changePercent'] = (submit['change']/temp["1. open"]).toFixed(2);
-			submit['timestamp'] = currentDateTemp; //EDT?
+			submit['changePercent'] = (submit['change']/temp["1. open"] * 100).toFixed(2);
+			//submit['timestamp'] = currentDateTemp + " EDT"; //EDT?
 			submit['open'] = temp["1. open"];
+			submit['low'] = temp["3. low"];
+			submit['high'] = temp["2. high"];
+			submit['lastPrice'] = temp["4. close"];
+
+			if (currentDateTemp.length == 10 || (currentDateTemp.indexOf(closedTime) !== -1)) {
+				submit['close'] = temp["4. close"]; //already closed today
+				submit['timestamp'] = currentDate + " " + closedTime + " EDT"; //EDT?
+			} else {
+				submit['close'] = previousDate["4. close"]; // today not closed yet
+				submit['timestamp'] = currentDateTemp + " EDT"; //EDT?
+			}
+
 			$scope.csDetails = submit;
 			$scope.quoted = true;
 		});
-
-			// var newestTemp = res.data["Meta Data"]["3. Last Refreshed"];
-			// var newest = newestTemp.substring(0, 9);
-			// var newestInfo = res.data["Time Series (Daily)"][newest];
-			// var leftTable = {};
-			// leftTable['Stock Ticker Symbol'] = $scope.searchText;
-			// leftTable['Last Price'] = newestInfo["4. close"];
-			// leftTable['Change (Change Percent)'] = (newestInfo["4. close"] - newestInfo["1. open"]).toFixed(2); 
-
-
-		// });
-
 	};
 
 	$scope.searchTextChange = function(text) {
